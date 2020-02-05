d158ccc67090be093b1482b41d06d3b28b3d2224
Szofi <nyafologus@gmail.com>
2020-02-05 15:30:35 +0100

    Add initialWishes

--- src/Components/WishList.js
+++ src/Components/WishList.js
@@ -1,10 +1,43 @@
-import React from "react";
-import Paper from "@material-ui/core/Paper";
+import React, { useState } from "react";
 import AppBar from "@material-ui/core/AppBar";
+import Paper from "@material-ui/core/Paper";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 
 export default function WishList() {
+  const initialWishes = [
+    {
+      id: 1,
+      content: "Audio-Technica ATH-M50x Headphones",
+      done: false,
+      link:
+        "https://www.audio-technica.com/cms/headphones/99aff89488ddd6b1/index.html"
+    },
+    {
+      id: 2,
+      content: "Celseste Mountain on Steam",
+      done: false,
+      link: "https://store.steampowered.com/app/504230/Celeste/"
+    },
+
+    {
+      id: 3,
+      content: "Dreamland Alaskan Husky Faux Fur Heated Throw",
+      done: false,
+      link:
+        "http://www.dreamlandworld.com/products/relaxwell-faux-fur-heated-throw/"
+    },
+    {
+      id: 4,
+      content: "House of Leaves",
+      done: true,
+      link:
+        "https://www.barnesandnoble.com/w/house-of-leaves-mark-z-danielewski/1103027816#/"
+    }
+  ];
+
+  const [wishes, setWishes] = useState(initialWishes);
+
   return (
     <Paper
       style={{
