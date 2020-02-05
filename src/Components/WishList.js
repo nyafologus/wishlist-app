4a675428ab42f5eceed559d9103b02c665472f6f
Szofi <nyafologus@gmail.com>
2020-02-05 12:35:57 +0100

    Add material-ui Paper component

--- src/Components/WishList.js
+++ src/Components/WishList.js
@@ -1,5 +1,19 @@
 import React from "react";
+import Paper from "@material-ui/core/Paper";
 
 export default function WishList() {
-  return <div>WishList Component!</div>;
+  return (
+    <Paper
+      style={{
+        height: "100vh",
+        backgroundImage:
+          "linear-gradient(to right top, #d1bebc, #d2c0c1, #c5b0ba, #b2a3b6, #9798b2, #768faa)",
+        padding: 0,
+        margin: 0
+      }}
+      elevation={0}
+    >
+      {/* use Paper Component as a reusable container to provide us with a background color */}
+    </Paper>
+  );
 }
