5201e050b500d19d066d9d3519d6ea61a2640f74
Szofi <nyafologus@gmail.com>
2020-02-05 11:48:11 +0100

    Init WishList component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import WishList from "./Components/WishList";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <WishList />;
 }
 
 export default App;
