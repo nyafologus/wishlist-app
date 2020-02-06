18dc925b709aa93a9bbcf449a7abfb39c4dfbe41
Szofi <nyafologus@gmail.com>
2020-02-06 21:37:34 +0100

    Embed material-ui List elements

--- src/Components/WishList.js
+++ src/Components/WishList.js
@@ -1,11 +1,19 @@
 import React from "react";
+import Paper from "@material-ui/core/Paper";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
 
 export default function WishList({ wishes }) {
   return (
-    <div>
-      {wishes.map(wish => (
-        <li key={wish.id}>{wish.content}</li>
-      ))}
-    </div>
+    <Paper>
+      <List>
+        {wishes.map(wish => (
+          <ListItem key={wish.id}>
+            <ListItemText>{wish.content}</ListItemText>
+          </ListItem>
+        ))}
+      </List>
+    </Paper>
   );
 }
