4bb6425758740dfc7b37756efd0077906d1d6b69
Szofi <nyafologus@gmail.com>
2020-02-06 21:39:11 +0100

    Add Divider element

--- src/Components/WishList.js
+++ src/Components/WishList.js
@@ -3,15 +3,19 @@ import Paper from "@material-ui/core/Paper";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
+import Divider from "@material-ui/core/Divider";
 
 export default function WishList({ wishes }) {
   return (
     <Paper>
       <List>
         {wishes.map(wish => (
-          <ListItem key={wish.id}>
-            <ListItemText>{wish.content}</ListItemText>
-          </ListItem>
+          <>
+            <ListItem key={wish.id}>
+              <ListItemText>{wish.content}</ListItemText>
+            </ListItem>
+            <Divider />
+          </>
         ))}
       </List>
     </Paper>
