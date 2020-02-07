4a2eb43cf926bae2a6bb062ef57bcb60f63da4e3
Szofi <nyafologus@gmail.com>
2020-02-07 07:00:48 +0100

    Add Paper and TextField

--- src/Components/WishForm.js
+++ src/Components/WishForm.js
@@ -1,5 +1,11 @@
 import React from "react";
+import Paper from "@material-ui/core/Paper";
+import TextField from "@material-ui/core/TextField";
 
 export default function WishForm() {
-  return <div>Wish Form</div>;
+  return (
+    <Paper>
+      <TextField />
+    </Paper>
+  );
 }
