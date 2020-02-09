f55413c043acbc24a288d0d1c8abc37948c308ec
Szofi <nyafologus@gmail.com>
2020-02-09 11:24:51 +0100

    Display checkbox

--- src/Components/Wish.js
+++ src/Components/Wish.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
+import Checkbox from "@material-ui/core/Checkbox";
 
 export default function Wish({ content, done }) {
   return (
     <ListItem>
+      <Checkbox checked={done} />
       <ListItemText>{content}</ListItemText>
     </ListItem>
   );
