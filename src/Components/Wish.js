91df842156b565ff80336d10e9bace9d8d820e5c
Szofi <nyafologus@gmail.com>
2020-02-09 11:20:20 +0100

    Extract remaining Wish component related elements from WishList

--- src/Components/Wish.js
+++ src/Components/Wish.js
@@ -1,5 +1,11 @@
 import React from "react";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
 
 export default function Wish({ content, done }) {
-  return <li>{content}</li>;
+  return (
+    <ListItem>
+      <ListItemText>{content}</ListItemText>
+    </ListItem>
+  );
 }
--- src/Components/WishList.js
+++ src/Components/WishList.js
@@ -2,8 +2,6 @@ import React from "react";
 import Wish from "./Wish";
 import Paper from "@material-ui/core/Paper";
 import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
 import Divider from "@material-ui/core/Divider";
 
 export default function WishList({ wishes }) {
@@ -12,11 +10,7 @@ export default function WishList({ wishes }) {
       <List>
         {wishes.map(wish => (
           <>
-            <ListItem>
-              <ListItemText>
-                <Wish content={wish.content} key={wish.id} done={wish.done} />
-              </ListItemText>
-            </ListItem>
+            <Wish content={wish.content} key={wish.id} done={wish.done} />
             <Divider />
           </>
         ))}
