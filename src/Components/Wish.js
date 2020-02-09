de1d3611029a135b42d3890cc1b59c5467800738
Szofi <nyafologus@gmail.com>
2020-02-09 14:14:54 +0100

    Import and add Delete IconButton

--- src/Components/Wish.js
+++ src/Components/Wish.js
@@ -4,6 +4,9 @@ import ListItemText from "@material-ui/core/ListItemText";
 import Checkbox from "@material-ui/core/Checkbox";
 import FormGroup from "@material-ui/core/FormGroup";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
+import IconButton from "@material-ui/core/IconButton";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import DeleteIcon from "@material-ui/icons/Delete";
 import Favorite from "@material-ui/icons/Favorite";
 import FavoriteBorder from "@material-ui/icons/FavoriteBorder";
 
@@ -22,6 +25,11 @@ export default function Wish({ content, done }) {
         />
       </FormGroup>
       <ListItemText>{content}</ListItemText>
+      <ListItemSecondaryAction>
+        <IconButton>
+          <DeleteIcon />
+        </IconButton>
+      </ListItemSecondaryAction>
     </ListItem>
   );
 }
