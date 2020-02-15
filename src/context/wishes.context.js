a0565202e3486f22f8008efcbb5fb1deb244f675
Szofi <nyafologus@gmail.com>
2020-02-16 00:09:36 +0100

    Add default wishes

--- src/context/wishes.context.js
+++ src/context/wishes.context.js
@@ -1,7 +1,19 @@
 import React, { createContext } from "react";
 
 const defaultWishes = [
-  //
+  {
+    id: uuid(),
+    content: "Audio-Technica ATH-M50x Headphones",
+    done: false,
+    link:
+      "https://www.audio-technica.com/cms/headphones/99aff89488ddd6b1/index.html"
+  },
+  {
+    id: uuid(),
+    content: "Celeste Mountain on Steam",
+    done: false,
+    link: "https://store.steampowered.com/app/504230/Celeste/"
+  }
 ];
 
 export const WishesContext = createContext();
