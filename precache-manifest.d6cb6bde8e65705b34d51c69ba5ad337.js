250b9dc67b21ceffa84d787110f9b36ebe49d437
Szofi <nyafologus@gmail.com>
2020-07-06 17:32:58 +0200

    Updates

---
+++ precache-manifest.d6cb6bde8e65705b34d51c69ba5ad337.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de30734176f4eeec6e6ce227f310074c",
+    "url": "/wishlist-app/index.html"
+  },
+  {
+    "revision": "7f7078f46463424a0e0f",
+    "url": "/wishlist-app/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "738d267cd9b009bf9d08",
+    "url": "/wishlist-app/static/js/2.d7db0bef.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/wishlist-app/static/js/2.d7db0bef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f7078f46463424a0e0f",
+    "url": "/wishlist-app/static/js/main.21329762.chunk.js"
+  },
+  {
+    "revision": "3d3ee979330044e95470",
+    "url": "/wishlist-app/static/js/runtime-main.895ad469.js"
+  }
+]);
